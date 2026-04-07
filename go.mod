df1835e1ba5baeca8dbb3597ea5d21eb96230899
fenezglonxiy <thdathcmus@gmail.com>
2026-04-07 09:51:59 +0700

    v1.0.1 commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fenezglonxiy/hello_go_module
+module github.com/fenezglonxiy/hello_go_module/v1.0.1
 
 go 1.25.5
--- hello_go_module.go
+++ hello_go_module.go
@@ -1,5 +1,10 @@
 package hello_go_module
 
+// Add takes in two int parameters and returns the sum of the parameters.
+//
+// Try it out: [mathisfun/addition]
+//
+// [mathisfun/addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
