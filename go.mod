e5bf80caf00ae055139d482fb3205022bcd8281a
fenezglonxiy <thdathcmus@gmail.com>
2026-04-07 10:11:47 +0700

    v2.0.0 commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/fenezglonxiy/hello_go_module/v1.0.1
+module github.com/fenezglonxiy/hello_go_module/v2.0.0
 
 go 1.25.5
+
+require golang.org/x/exp v0.0.0-20260312153236-7ab1446f8b90
--- hello_go_module.go
+++ hello_go_module.go
@@ -1,10 +1,16 @@
 package hello_go_module
 
-// Add takes in two int parameters and returns the sum of the parameters.
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes in two [Number] parameters and returns the sum of the parameters.
 //
 // Try it out: [mathisfun/addition]
 //
 // [mathisfun/addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[N Number](a, b N) N {
 	return a + b
 }
